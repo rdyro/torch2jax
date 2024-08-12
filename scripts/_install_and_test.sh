91508667e10e8defed02a5d26cddee36d7f54a93
Robert Dyro <rdyro@stanford.edu>
2024-08-11 17:59:17 -0700

    removing flaky tests

--- scripts/_install_and_test.sh
+++ scripts/_install_and_test.sh
@@ -11,6 +11,7 @@ pyenv global $PYTHON_VER
 pip install -U uv > /dev/null 2> /dev/null
 uv pip install --system --cache-dir $HOME/.cache/uv torch pytest ruff \
   "jax[$JAX_OPT]==$JAX_VERSION" 2> /dev/null
+uv pip install --system "numpy==1.26.4" 2> /dev/null # downgrade numpy to ver 1
 uv pip install --system .  2> /dev/null
 
 echo "JAX_VERSION=$JAX_VERSION" "JAX_OPT=$JAX_OPT" "########################"
--- scripts/test_in_docker.sh
+++ scripts/test_in_docker.sh
@@ -2,27 +2,24 @@
 
 set -xe
 
-root_dir="$(realpath $(pwd))"
+#local_cache_dir="$(realpath $(pwd))/.cache/uv"
+local_cache_dir="$HOME/.cache/uv"
 
 python_ver=("3.9" "3.10" "3.11" "3.12")
 jax_ver=("0.4.26" "0.4.27" "0.4.28" "0.4.29" "0.4.30" "0.4.31")
 
-
 # cpu tests ####################################################################
-#image_name="t2j:cpu"
-#if [[ -z "$(docker images -q $image_name 2> /dev/null)" ]]; then
-#  docker build -t $image_name \
-#    --build-arg=IMAGE_NAME="python:3.9" .
-#fi
-#
-## run actual tests
-#for python_ver in ${python_ver[@]}; do
-#  for jax_ver in ${jax_ver[@]}; do
-#    docker run --rm \
-#      -v "$root_dir/.cache/uv:/root/.cache/uv" \
-#      "$image_name" ./scripts/_install_and_test.sh $python_ver $jax_ver "cpu"
-#  done
-#done
+image_name="t2j:cpu"
+docker build -t $image_name --build-arg=IMAGE_NAME="python:3.9" .
+
+# run actual tests
+for python_ver in ${python_ver[@]}; do
+  for jax_ver in ${jax_ver[@]}; do
+    docker run --rm \
+      -v "$local_cache_dir:/root/.cache/uv" \
+      "$image_name" ./scripts/_install_and_test.sh $python_ver $jax_ver "cpu"
+  done
+done
 
 # cuda tests ###################################################################
 if [[ -z "$(command -v nvidia-smi)" ]]; then
@@ -30,20 +27,16 @@ if [[ -z "$(command -v nvidia-smi)" ]]; then
   exit 0
 fi
 
-python_ver=("3.9" "3.10" "3.11" "3.12")
 jax_ver=("0.4.30" "0.4.31")
 
 image_name="t2j:cuda"
-if [[ -z "$(docker images -q $image_name 2> /dev/null)" ]]; then
-  docker build -t $image_name \
-    --build-arg=IMAGE_NAME="ghcr.io/nvidia/jax:base" .
-fi
+docker build -t $image_name --build-arg=IMAGE_NAME="ghcr.io/nvidia/jax:base" .
 
 # run actual tests
 for python_ver in ${python_ver[@]}; do
   for jax_ver in ${jax_ver[@]}; do
     docker run --rm \
-      -v "$root_dir/.cache/uv:/root/.cache/uv" \
+      -v "$local_cache_dir:/root/.cache/uv" \
       --gpus all --shm-size=1g \
       "$image_name" ./scripts/_install_and_test.sh $python_ver $jax_ver "cuda"
   done
